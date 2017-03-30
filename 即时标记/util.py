68c58240c0eb763d0925b1b16b1513e59d334a90
daize.song <daize.song@fairlinkcentury.com>
2017-03-30 18:34:31 +0800

    update

---
+++ 即时标记/util.py
@@ -0,0 +1,21 @@
+#! python3
+
+def lines(file):
+    for line in file:
+        yield line
+    yield '\n'
+
+def blocks(file):
+    block = []
+    for line in lines(file):
+        if line.strip():
+            block.append(line)
+        elif block:
+            yield ''.join(block).strip()
+            block = []
+
+lines('text_input.txt')
+blocks('text_input.txt')
+
+
+
