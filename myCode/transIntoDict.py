5c25593935c8be7397d172de9d701fd55974e01a
daize.song <daize.song@fairlinkcentury.com>
2017-05-19 16:33:51 +0800

    update

---
+++ myCode/transIntoDict.py
@@ -0,0 +1,18 @@
+
+def combineIntoDict(ip='1-ip.txt', name='1-name.txt'):
+    fileDict = {}
+    fileName = []
+    fileIp = []
+    with open('C:\\Users\\daize\\Desktop\\%s' % ip) as serverIps:
+        for ip in serverIps.readlines():
+            fileIp.append(ip.strip('\n, '))
+    with open('C:\\Users\\daize\\Desktop\\%s' % name) as serverNames:
+        for name in serverNames.readlines():
+            fileName.append(name.strip('\n, '))
+
+    for i in range(len(fileIp)):
+        fileDict[fileIp[i]] = fileName[i]
+    return fileDict
+
+if __name__ == '__main__':
+    print(combineIntoDict('1-ip.txt', '1-name.txt'))
